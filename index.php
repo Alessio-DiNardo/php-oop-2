cc3cda38cec3fb598b9ea6c3f3bd79a2fed223f2
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-07-14 16:36:16 +0200

    Add bootstrap

--- index.php
+++ index.php
@@ -10,7 +10,9 @@ Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tom and jerry </title>
+    <title>Tom and Jerry </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    
 </head>
 <body>
     
