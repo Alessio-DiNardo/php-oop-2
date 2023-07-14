6365deb75106e136fe30682c5debc29876999098
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-07-14 15:08:18 +0200

    Add product with class and function construct and function

---
+++ classes/Product.php
@@ -0,0 +1,41 @@
+<?php 
+
+class product{
+    protected $name;
+    protected $brand;
+    protected $price;
+    protected $image;
+    protected $category;
+
+    public function __construct(String $name, String $brand, Int $price, String $image, String $category){
+        $this->setName($name);
+        $this->setBrand($brand);
+        $this->setPrice($price);
+        $this->setImage($image);
+        $this->setCategory($category);
+    }
+
+    public function setName($name){
+        $this->name = $name;
+    }
+    public function setBrand($brand){
+        $this->brand = $brand;
+    }
+    public function setprice($price){
+        $this->price = $price;
+    }
+    public function setImage($image){
+        $this->image = $image;
+    }
+    public function setCategory($category){
+        $this->category = $category;
+    }
+
+
+
+}
+
+
+
+
+?>
