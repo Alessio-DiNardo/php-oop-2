dd4f4aea99df7bb8f41375be8bafad1e745de0b2
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-07-14 16:32:09 +0200

    add extends on food

--- classes/Food.php
+++ classes/Food.php
@@ -1,19 +1,27 @@
 
 <?php 
 include_once __DIR__ . '/Product.php';
-class Food {
+class Food extends Product{
     public $type;
     public $ingredient;
     public $expiration;
 
-
-
-    public function __construct(String $type, String $ingredient, String $expiration, ) {
-        $this->type = $type;
-        $this->ingredient = $ingredient;
-        $this->expiration = $expiration;
+    public function __construct(String $name, String $brand, Int $price, String $image, String $category,String $type, String $ingredient, String $expiration ) {
+        parent::__construct($name,$brand,$price,$image,$category);
+        $this->setType($type);
+        $this->setIngredient($ingredient);
+        $this->setExpiration($expiration);
     }
 
+    public function setType($type){
+        $this->type = $type;
+    }
+    public function setIngredient($ingredient){
+        $this->ingredient = $ingredient;
+    }
+    public function setExpiration($expiration){
+        $this->expiration = $expiration;
+    }
 }
 
 
